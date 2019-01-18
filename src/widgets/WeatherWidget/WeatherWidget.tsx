2bd650527a7d560b5e5c8d4056dd2ebe8eaa664f
Mats Roshauw <karmats@gmail.com>
2019-01-17 20:17:20 -0800

    Basic layout for weather widget

--- src/widgets/WeatherWidget/WeatherWidget.tsx
+++ src/widgets/WeatherWidget/WeatherWidget.tsx
@@ -1,3 +1,25 @@
 import React from 'react';
+import sunnyAnimated from './svgs/animated/day.svg';
+import sunny from './svgs/static/day.svg';
+import './WeatherWidget.css';
 
-export default () => <div>Hello weather widget</div>;
+export default () => (
+  <div>
+    <img className="WeatherWidget-svg" src={sunnyAnimated} />
+    <p className="WeatherWidget-desc">15°</p>
+    <div className="WeaterWidget-footer">
+      <div>
+        <img src={sunny} />
+        <p>12°</p>
+      </div>
+      <div>
+        <img src={sunny} />
+        <p>12°</p>
+      </div>
+      <div>
+        <img src={sunny} />
+        <p>12°</p>
+      </div>
+    </div>
+  </div>
+);
