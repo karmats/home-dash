bcfda3590d338bf5e41673ab6450ae7dace1968e
Mats Roshauw <karmats@gmail.com>
2019-06-16 18:14:39 +0200

    Weather data with server sent events

--- server/controllers/WeatherController.ts
+++ server/controllers/WeatherController.ts
@@ -3,16 +3,24 @@ import { getWeatherForecasts } from '../services';
 import { defaultHeaders } from '../utils';
 
 // Every 5 minute
-const FORECAST_REFRESH_INTERVAL = 5 * 60 * 60 * 1000;
-
-export const getForecasts = (lat: number, lon: number) => getWeatherForecasts(lat, lon);
+const FORECAST_REFRESH_INTERVAL = 5 * 60 * 1000;
 
 export const getForecastsFromRequest = function(req: express.Request, res: express.Response) {
-  const { lat, lon } = req.query;
+  const { lat, lon, sse } = req.query;
   if (!lat || !lon) {
     res.writeHead(400);
-    res.write(`Parameters lat and lon needs to be supplied. Supplied lat: ${lat}, lon: ${lon}`);
+    res.write('Parameters lat and lon needs to be supplied.');
     res.end();
+  } else if (sse) {
+    // Sse requested, keep connection open and feed with weather data
+    res.writeHead(200, {
+      ...defaultHeaders,
+      'Content-Type': 'text/event-stream',
+      'Cache-Control': 'no-cache',
+      Connection: 'keep-alive'
+    });
+    pollForecasts(+lat, +lon, res);
+    res.on('close', () => stopPollForecast());
   } else {
     getWeatherForecasts(+lat, +lon)
       .then(forecasts => {
@@ -29,9 +37,14 @@ export const getForecastsFromRequest = function(req: express.Request, res: expre
   }
 };
 
-let intervalId = -1;
-export const pollForecasts = (lat: number, lon: number) => {
-  intervalId = setInterval(getForecasts, FORECAST_REFRESH_INTERVAL, lat, lon);
+let timer = -1;
+export const pollForecasts = (lat: number, lon: number, res: express.Response) => {
+  const pollFn = (lat: number, lon: number, res: express.Response) => {
+    getWeatherForecasts(+lat, +lon).then(forecasts => {
+      res.write(`data:${JSON.stringify(forecasts)}\n\n`);
+    });
+  };
+  timer = setInterval(pollFn, FORECAST_REFRESH_INTERVAL, lat, lon, res);
 };
 
-export const stopPollForecast = () => clearInterval(intervalId);
+export const stopPollForecast = () => clearInterval(timer);
