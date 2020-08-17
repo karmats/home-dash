e8d0e35ff7c884aebd8523b7c4eaf32d0e70310f
Mats Roshauw <karmats@gmail.com>
2020-08-17 08:55:03 +0200

    Calendar server test

---
+++ server/modules/Calendar/Calendar.test.ts
@@ -0,0 +1,56 @@
+import CalendarController from './Calendar.controller';
+import { DEFAULT_HEADERS } from '../../utils';
+import { CalendarEvent } from '../../../shared/types';
+
+const MOCK_CALENDAR_BY_DATES_EVENTS: CalendarEvent[] = [
+  {
+    from: { date: '2020-08-20' },
+    to: { date: '2020-08-21' },
+    summary: 'Calendar by dates',
+  },
+];
+const MOCK_NEXT_CALENDAR_EVENTS: CalendarEvent[] = [
+  {
+    from: { date: '2020-08-20' },
+    to: { date: '2020-08-21' },
+    summary: 'Next calendar events',
+  },
+];
+
+jest.mock('../Authentication', () => ({
+  AuthenticationService: {
+    isConnectedToGoogle: () => Promise.resolve(true),
+  },
+}));
+jest.mock('./Calendar.service', () => ({
+  __esModule: true,
+  default: {
+    getCalendarEventsByDates: () => Promise.resolve(MOCK_CALENDAR_BY_DATES_EVENTS),
+    getNextCalendarEvents: () => Promise.resolve(MOCK_NEXT_CALENDAR_EVENTS),
+  },
+}));
+
+describe('Calendar server', () => {
+  describe('Controller', () => {
+    it('calls service on correct request', () => {
+      const req: any = {
+        query: {
+          next: '10',
+        },
+      };
+      const res: any = {
+        writeHead: (status: number, headers) => {
+          expect(status).toBe(200);
+          expect(headers).toEqual(DEFAULT_HEADERS);
+        },
+        write: body => {
+          expect(body).toEqual(JSON.stringify(MOCK_NEXT_CALENDAR_EVENTS));
+          return true;
+        },
+        end: () => {},
+      };
+
+      CalendarController.getCalendarEventsFromRequest(req, res);
+    });
+  });
+});
