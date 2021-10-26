2366defa42dba65c25c068e1922be0739a1b477d
Mats Roshauw <karmats@gmail.com>
2021-10-26 15:58:22 +0200

    Show news title, image and description

--- client/widgets/News/News.test.tsx
+++ client/widgets/News/News.test.tsx
@@ -21,11 +21,53 @@ jest.mock('../../hooks', () => ({
   useEventSourceWithRefresh: () => mockUseEventSourceWithRefresh,
 }));
 
+jest.useFakeTimers();
+
 describe('News', () => {
   describe('Component', () => {
     afterEach(cleanup);
     it('renders', () => {
       render(<NewsComponent />);
+    });
+
+    it('renders indication that news are fetching', () => {
+      const { getByText } = render(<NewsComponent />);
+      const indicator = getByText('Loading...');
+      expect(indicator).toBeDefined();
+    });
+
+    it('renders title, image and description', () => {
+      const newsData: News = {
+        image: '',
+        language: 'sv',
+        source: 'https://dn.se',
+        url: 'https://dn.se',
+        items: [
+          {
+            date: new Date().toISOString(),
+            link: 'https://dn.se',
+            title: 'Something bad happened',
+            content: 'Yes its true something bad has happened',
+            media: {
+              description: 'Look at this image',
+              type: 'image/jpg',
+              url: 'https://dn.se',
+            },
+          },
+        ],
+      };
+      mockUseEventSourceWithRefresh = {
+        data: newsData,
+        refreshData: () => {},
+        updateData: () => {},
+      };
+
+      const { getByText, getByAltText } = render(<NewsComponent />);
+
+      const firstNewsData = newsData.items[0];
+      expect(getByText(firstNewsData.title)).toBeDefined();
+      expect(getByText(firstNewsData.content!)).toBeDefined();
+      expect(getByAltText(firstNewsData.media!.description)).toBeDefined();
     });
   });
 });
--- client/widgets/News/News.tsx
+++ client/widgets/News/News.tsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Spinner from '../../components/Spinner/Spinner';
 import { News } from '../../../shared/types';
 import api from '../../apis/Api';
 import { useEventSourceWithRefresh } from '../../hooks';
 import './News.css';
+
+// Every 10 second
+const TIME_REFRESH_INTERVAL = 5 * 1000;
 
 const newsEventSourceConfig = {
   eventSource: api.getLatestNewsEventSource(),
@@ -14,13 +17,27 @@ export default function () {
     newsEventSourceConfig,
     api.getLatestNews
   );
+  const [currentNewsIdx, setCurrentNewsIdx] = useState<number>(0);
+  useEffect(() => {
+    const updateCurrentNews = () => setCurrentNewsIdx((currentNewsIdx + 1) % news.items.length);
+    const timeInterval = setInterval(updateCurrentNews, TIME_REFRESH_INTERVAL);
+    return () => {
+      clearInterval(timeInterval);
+    };
+  }, [currentNewsIdx, news]);
 
-  return news && news.items.length ? (
-    <ul className="News-main" onClick={refreshNews}>
-      {news.items.map(n => (
-        <li key={n.date}>{n.title}</li>
-      ))}
-    </ul>
+  const currentNews = news.items[currentNewsIdx];
+  const content = currentNews?.content || currentNews?.media?.description;
+  return currentNews ? (
+    <div className="News-main" onClick={refreshNews}>
+      <h1 className="News-header">{currentNews.title}</h1>
+      <div className="News-content">
+        {currentNews.media ? (
+          <img className="News-image" src={currentNews.media.url} alt={currentNews.media.description}></img>
+        ) : null}
+        {content ? <p className="News-description">{content}</p> : null}
+      </div>
+    </div>
   ) : (
     <Spinner />
   );
