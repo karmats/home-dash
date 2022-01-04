f35fe3aae81600396ff2a596a03b77e05356bc72
Mats Roshauw <karmats@gmail.com>
2022-01-04 10:32:51 +0100

    Ssl support

--- server/server.ts
+++ server/server.ts
@@ -1,6 +1,9 @@
 import './env';
 import express from 'express';
 import addRequestId from 'express-request-id';
+import fs from 'fs';
+import http from 'http';
+import https from 'https';
 import { GoogleAuthenticatorController, SectorAlarmAuthenticationController } from './modules/Authentication';
 import { WeatherController } from './modules/Weather';
 import { CalendarController } from './modules/Calendar';
@@ -8,9 +11,6 @@ import { TemperatureController } from './modules/Temperature';
 import { HomeAlarmController } from './modules/HomeAlarm';
 import { NewsController } from './modules/News';
 import { getLogger } from './logger';
-
-const hostname = '0.0.0.0';
-const port = 4000;
 
 const logger = getLogger('server');
 const app = express();
@@ -29,6 +29,26 @@ app.post('/homealarm/toggle', HomeAlarmController.toggleAlarm);
 app.get('/auth/google', GoogleAuthenticatorController.authenticateToGoogle);
 app.get('/auth/sectoralarm', SectorAlarmAuthenticationController.authenticateToSectorAlarm);
 
-app.listen(port, hostname, function () {
-  logger.info(`Server running at http://${hostname}:${port}/`);
+// Http
+const hostname = '0.0.0.0';
+const port = 4000;
+http.createServer(app).listen(port, hostname, () => {
+  logger.info(`Server running at http://${hostname}:${port}`);
 });
+
+// Https
+const portSsl = 4001;
+let key: Buffer | null = null;
+let cert: Buffer | null = null;
+try {
+  key = fs.readFileSync(`${__dirname}/../key.pem`);
+  cert = fs.readFileSync(`${__dirname}/../certificate.pem`);
+} catch (e) {
+  logger.info(`Couldn't find https certificates, starting http server only: ${JSON.stringify(e)}`);
+}
+
+if (key && cert) {
+  https.createServer({ key, cert }, app).listen(portSsl, hostname, () => {
+    logger.info(`Server running at https://${hostname}:${portSsl}`);
+  });
+}
